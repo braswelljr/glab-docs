795b540866baec24943b0334497a72db15511c95
braswelljr <braswellkenneth7@gmail.com>
2021-05-16 19:19:47 +0000

    Update Index

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-// const withPWA = require("next-pwa");
-// const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 
 module.exports = withPlugins(
   [
-    // [
-    //   withPWA,
-    //   {
-    //     pwa: {
-    //       dest: "public",
-    //       runtimeCaching
-    //     }
-    //   }
-    // ],
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+          runtimeCaching
+        }
+      }
+    ],
     [withImages]
   ],
   {
