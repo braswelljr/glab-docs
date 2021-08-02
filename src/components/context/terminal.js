cf7943a1c27362888ac1e2aa87946d98eb55d25a
braswelljr <braswellkenneth7@gmail.com>
2021-08-02 00:58:29 +0000

    Fix index terminal context:links

--- src/components/context/terminal.js
+++ src/components/context/terminal.js
@@ -3,19 +3,19 @@ export const terminalD = [
     title: `Your entire GitLab workflow`,
     workflow: `Work with issues, merge requests, GitLab CI pipelines and jobs, releases and more`,
     name: `View all glab commands`,
-    link: `#`
+    link: `/docs/commands`
   },
   {
     title: `Script and customize`,
     workflow: `Call the GitLab API to script almost any action, and set a custom alias for any command`,
     name: `Learn about aliases and API`,
-    link: `#`
+    link: `/docs/commands/alias`
   },
   {
     title: `Supports all GitLab instances`,
     workflow: `Available for repositories hosted on GitLab.com and self-hosted GitLab Instances`,
     name: `Learn how to authenticate`,
-    link: `#`
+    link: `/docs/commands/auth`
   },
   {
     title: `We <3 Community`,
