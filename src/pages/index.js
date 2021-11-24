417f3a6a500a89f69e3b7a467b8f98fef235a228
braswelljr <braswellkenneth7@gmail.com>
2021-11-24 06:55:20 +0000

    Update Deepsource fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function Index() {
           GLab is an open source GitLab CLI tool bringing GitLab to your
           terminal next to where you are already working with git and your code.
         </p>
-        <Link href="/docs">
+        <Link href="/docs" passHref>
           <button
             type="button"
             className={clsx(
